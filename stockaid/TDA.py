5471e16b09b103d9615e58955968563babd1b355
jessedutton <jessedutton@gmail.com>
2022-08-25 17:50:33 -0600

    copyright in the header

--- stockaid/TDA.py
+++ stockaid/TDA.py
@@ -1,3 +1,18 @@
+
+# Copyright 2022 Jesse Dutton
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """This is the implementation of API calls to TD Ameritrade for getting quotes,
    historical data, and option chains. The common use cases are included. The
    TDA API is capable of much that is not included here. Completeness is not a
--- stockaid/greeks/__init__.py
+++ stockaid/greeks/__init__.py
@@ -1,3 +1,18 @@
+
+# Copyright 2022 Jesse Dutton
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """A collection of utils for applying the option Greeks to our predictions
    about the future. Specifically, we can look at how the price of an option
    is expected to change based on a predicted change in the price of the
